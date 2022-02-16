b16b5b7f3790b54427d8d20db221407041307fb4
D4ness <lyakhov016@gmail.com>
2022-02-16 18:48:53 +0300

    fix block position, and condition onpointerup

--- scripts/dragNDrop.js
+++ scripts/dragNDrop.js
@@ -15,22 +15,29 @@ takeField.onpointerdown = function (event) {
     const block = document.createElement('div');
     block.className = 'square';
     const color = generateColor();
-    console.log(color);
     block.style.zIndex = 100;
-    block.style.left = cordX - 50 + 'px';
-    block.style.top = cordY - 50 + 'px';
     block.style.backgroundColor = color;
     takeField.appendChild(block);
+    block.style.left = cordX - block.offsetWidth / 2 + 'px';
+    block.style.top = cordY - block.offsetHeight / 2 + 'px';
     document.onpointermove = (event) => {
-        block.style.left = event.clientX - 50 + 'px';
-        block.style.top = event.clientY - 50 + 'px';
+        block.style.left = event.clientX - block.offsetWidth / 2 + 'px';
+        block.style.top = event.clientY - block.offsetHeight / 2 + 'px';
     }
     document.onpointerup = function (event) {
-        const cordX = event.clientX;
-        const cordY = event.clientY;
-        if ((cordX > 1200) || (cordY > 400) || (cordX < 400)) {
-            takeField.removeChild(block);
-        } else if ((cordY < 400) && (cordX > 400) && (cordX < 800)) {
+        function checkPutField(x, y) {
+            return (cordSquareX > x) && (cordSquareX < x + 400) &&
+                (cordSquareY > y) && (cordSquareY < y + 400)
+        }
+
+        const cordSquareX = event.clientX;
+        const cordSquareY = event.clientY;
+        const cordPutNetX = document.querySelector('.put_net').offsetLeft;
+        const cordPutNetY = document.querySelector('.put_net').offsetTop;
+        const cordPutHereX = document.querySelector('.put_here').offsetLeft;
+        const cordPutHereY = document.querySelector('.put_here').offsetTop;
+
+        if (checkPutField(cordPutNetX, cordPutNetY)) {
             if (netCounter++ < 16) {
                 block.style.position = 'relative';
                 block.style.left = null;
@@ -42,9 +49,11 @@ takeField.onpointerdown = function (event) {
             } else {
                 takeField.removeChild(block);
             }
-        } else if ((cordY < 400) && (cordX > 800) && (cordX < 1200)) {
+        } else if (checkPutField(cordPutHereX, cordPutHereY)) {
             takeField.removeChild(block);
             putHereField.appendChild(block);
+        } else {
+            takeField.removeChild(block);
         }
         document.onpointermove = undefined;
     };
